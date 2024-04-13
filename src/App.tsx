c2a22f98dad5fc4cdca773a3cedc93ad10472183
guridaek <xylitol79@gmail.com>
2024-04-13 14:05:29 +0900

    feat: MUI theme 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { ThemeProvider, createTheme } from "@mui/material";
 import { Outlet } from "react-router-dom";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // inhaDeepBlue
+      main: "#005BAC",
+      contrastText: "#fff",
+    },
+    secondary: {
+      // inhaSkyBlue
+      main: "#00AFEC",
+      contrastText: "#fff",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="max-w-xl mx-auto h-screen border border-sky-500">
-      <Outlet />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="max-w-xl mx-auto h-screen border border-sky-500">
+        <Outlet />
+      </div>
+    </ThemeProvider>
   );
 }
 
