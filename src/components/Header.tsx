62816d94f6f1d69c72e0440aa5ff17f9b828139d
guridaek <xylitol79@gmail.com>
2024-04-15 16:16:20 +0900

    feat: Header 구현

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import SearchSVG from "@assets/serach.svg?react";
+import ProfileSVG from "@assets/profile.svg?react";
+import { IconButton } from "@mui/material";
+
+function Header() {
+  return (
+    <header className="flex justify-between w-full h-16 sticky top-0 border-b border-inhaSkyBlue p-4 bg-white">
+      <a className="text-xl font-bold text-inhaDeepBlue" href="home">
+        HiBuddy
+      </a>
+      <div className="flex items-center gap-4">
+        <IconButton aria-label="search">
+          <SearchSVG />
+        </IconButton>
+        <IconButton aria-label="profile">
+          <ProfileSVG />
+        </IconButton>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
