19934da480f3ece29d8a053cc64d254d0e69326f
guridaek <xylitol79@gmail.com>
2024-05-01 16:55:14 +0900

    chore: utils alias path 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "/",
+  base: "/main",
   resolve: {
     alias: {
       "@components": "/src/components",
       "@assets": "/src/assets",
       "@pages": "/src/pages",
+      "@utils": "/src/utils",
     },
   },
 });
