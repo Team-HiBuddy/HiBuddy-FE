36bb3d4329a4bc61a37e195ba093fad816e049e9
guridaek <xylitol79@gmail.com>
2024-05-09 12:15:26 +0900

    feat: queryClient 추가

---
+++ src/hooks/query/queryClient.ts
@@ -0,0 +1,18 @@
+import { QueryCache, QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+    mutations: {
+      retry: 1,
+    },
+  },
+
+  queryCache: new QueryCache({
+    onError: (error) => {
+      alert(error.message);
+    },
+  }),
+});
