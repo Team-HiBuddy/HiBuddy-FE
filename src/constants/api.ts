37ffb20a10867729d85e2de8ff58a9d76792af78
guridaek <xylitol79@gmail.com>
2024-05-10 14:26:40 +0900

    feat: 카카오 로그인 query params 상수화

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -4,6 +4,12 @@ export const HIBUDDY_BASE_URL = "https://hibuddy/v1";
 
 export const KAKAO_CLIENT_ID = "badeb291be382d61336b18b4b854874b";
 
-export const KAKAO_REDIRECT_URI = "http://localhost:8080/hibuddy/v1/auth/kakao/callback";
+export const KAKAO_REDIRECT_URI = "http://localhost:8080/auth/kakao/callback";
 
 export const KAKAO_ACCESS_TOKEN_LOCAL_STORAGE_KEY = "kakaoAccessToken";
+
+export const KAKAO_LOGIN_PARAMS = new URLSearchParams({
+  response_type: "code",
+  client_id: KAKAO_CLIENT_ID,
+  redirect_uri: KAKAO_REDIRECT_URI,
+});
