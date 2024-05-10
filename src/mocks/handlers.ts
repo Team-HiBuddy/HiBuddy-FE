26874da3256228562c51ec9ce02d0113b93ae331
guridaek <xylitol79@gmail.com>
2024-05-10 15:49:00 +0900

    feat: 인기 스레드 목록 mocking

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,11 @@
 import { HIBUDDY_BASE_URL } from "@constants/api";
-import { http, HttpResponse } from "msw";
+import { delay, http, HttpResponse } from "msw";
+import popularThreads from "./data/popularThreads.json";
 
 export const handlers = [
   http.get(`${HIBUDDY_BASE_URL}/auth/kakao/login`, async () => {
+    await delay(3000);
+
     return new HttpResponse("OK", {
       status: 200,
       headers: {
@@ -10,5 +13,11 @@ export const handlers = [
           "30DKVAAsOX9ccZbtQycmvt5p3PEWVeLFpbQM1LYWDSscuqfUdo0_-dARI1QKPXSZAAABj2CxyI9Udd9ffL_GXA",
       },
     });
+  }),
+
+  http.get(`${HIBUDDY_BASE_URL}/posts/ranking`, async () => {
+    await delay(2000);
+
+    return HttpResponse.json(popularThreads);
   }),
 ];
