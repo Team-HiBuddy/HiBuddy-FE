81688f9ba5d8f970ec0a9d808aa7e6718f98e434
guridaek <xylitol79@gmail.com>
2024-05-11 12:10:20 +0900

    feat: 레이아웃 z-index 설정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,11 @@
 import SearchSVG from "@assets/serach.svg?react";
 import ProfileSVG from "@assets/profile.svg?react";
 import { IconButton } from "@mui/material";
-import { ROUTER_PATH } from "../router";
+import { ROUTER_PATH } from "../../router";
 
 function Header() {
   return (
-    <header className="flex justify-between w-full h-16 sticky top-0 border-b-2 border-inhaSkyBlue p-4 bg-white">
+    <header className="flex justify-between w-full h-16 sticky top-0 border-b-2 border-inhaSkyBlue p-4 bg-white z-10">
       <a className="text-xl font-bold text-inhaDeepBlue" href={ROUTER_PATH.MAIN}>
         HiBuddy
       </a>
