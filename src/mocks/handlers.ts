2685139bb71b8fa282b5398f1e4ae1f66bdee3d9
guridaek <xylitol79@gmail.com>
2024-05-11 17:40:37 +0900

    feat: 이미지 업로드 api mocking

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,8 @@
 import { HIBUDDY_BASE_URL } from "@constants/api";
 import { delay, http, HttpResponse } from "msw";
 import popularThreads from "./data/popularThreads.json";
+import successfulResponse from "./data/successfulResponse.json";
+import failedResponse from "./data/failedResponse.json";
 
 export const handlers = [
   http.get(`${HIBUDDY_BASE_URL}/auth/kakao/login`, async () => {
@@ -19,5 +21,24 @@ export const handlers = [
     await delay(2000);
 
     return HttpResponse.json(popularThreads);
+  }),
+
+  http.post(`${HIBUDDY_BASE_URL}/images/upload`, async ({ request }) => {
+    await delay(3000);
+
+    const data = successfulResponse;
+
+    const formData = await request.formData();
+    const files = formData.getAll("fileList");
+
+    if (files.length < 1) return HttpResponse.json(failedResponse);
+
+    const imageIds = Array.from(files).map((_, idx) => Date.now() + idx);
+
+    data.result = {
+      imageIds: imageIds,
+    };
+
+    return HttpResponse.json(data);
   }),
 ];
