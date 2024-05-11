67907cdb067e7da404cc0dbd759294fd8e5b584b
guridaek <xylitol79@gmail.com>
2024-05-11 12:08:33 +0900

    chore: vite port 8080으로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "/main",
+  server: {
+    port: 8080,
+  },
   resolve: {
     alias: {
       "@components": "/src/components",
