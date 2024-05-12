38959d0d6be108ee8560c7bb02691ca24e36ac6e
guridaek <xylitol79@gmail.com>
2024-05-12 14:19:01 +0900

    feat: access token 저장 및 불러오기

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,4 +1,20 @@
 import { http } from "@apis/axios";
+import { KAKAO_ACCESS_TOKEN_LOCAL_STORAGE_KEY } from "@constants/api";
+import { AxiosResponse } from "axios";
 
 export const issueLoginToken = async (authCode: string) =>
   await http.get(`/auth/kakao/login?code=${authCode}`);
+
+export const getAccessToken = () => localStorage.getItem(KAKAO_ACCESS_TOKEN_LOCAL_STORAGE_KEY);
+
+export const saveAccessToken = (response: AxiosResponse) => {
+  const jwt = response.headers["Authorization"];
+
+  if (!jwt) {
+    console.error("cat't find JWT.");
+
+    return;
+  }
+
+  localStorage.setItem(KAKAO_ACCESS_TOKEN_LOCAL_STORAGE_KEY, jwt.toString());
+};
--- src/apis/axios.tsx
+++ src/apis/axios.tsx
@@ -1,5 +1,6 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
-import { HIBUDDY_BASE_URL, KAKAO_ACCESS_TOKEN_LOCAL_STORAGE_KEY } from "@constants/api";
+import { HIBUDDY_BASE_URL } from "@constants/api";
+import { getAccessToken, saveAccessToken } from "./auth";
 
 const axiosInstance = axios.create({
   baseURL: HIBUDDY_BASE_URL,
@@ -10,19 +11,29 @@ const axiosInstance = axios.create({
 export interface HttpClient extends AxiosInstance {
   get<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<T>;
   post<T = unknown>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T>;
+  delete<T = unknown>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T>;
 }
 
 export const http: HttpClient = axiosInstance;
 
 http.interceptors.response.use(
   (response) => {
-    const jwt = response.headers.authorization;
-
-    if (jwt) {
-      localStorage.setItem(KAKAO_ACCESS_TOKEN_LOCAL_STORAGE_KEY, jwt.toString());
+    if (response.headers["Authorization"]) {
+      saveAccessToken(response);
     }
 
     return response.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+http.interceptors.request.use(
+  (request) => {
+    request.headers["Authorization"] = `Bearer ${getAccessToken()}`;
+
+    return request;
   },
   (error) => {
     return Promise.reject(error);
