65c8cd66ed7a1d29c724dabf14e73d649e15fb6d
guridaek <xylitol79@gmail.com>
2024-05-27 13:22:48 +0900

    feat: 빈 스레드 리스트 이미지

--- src/components/ThreadList.tsx
+++ src/components/ThreadList.tsx
@@ -4,13 +4,14 @@ import { useEffect, useRef } from "react";
 import BubbleLoadingSVG from "@assets/bubble-loading.svg?react";
 import { ThreadContents } from "@models/thread";
 import { UseInfiniteQueryResult } from "@tanstack/react-query";
+import emptyBox from "@assets/empty-box.png";
 
 interface Props {
   infiniteQueryResult: UseInfiniteQueryResult<ThreadContents[]>;
 }
 
 function ThreadList({ infiniteQueryResult }: Props) {
-  const { data: threadList, fetchNextPage, hasNextPage } = infiniteQueryResult;
+  const { data: threadList, fetchNextPage, hasNextPage, isPending } = infiniteQueryResult;
 
   const lastItemRef = useRef<HTMLElement>(null);
 
@@ -22,30 +23,39 @@ function ThreadList({ infiniteQueryResult }: Props) {
     }
   }, [isIntersecting, hasNextPage, fetchNextPage]);
 
-  return (
+  return isPending ? (
+    <BubbleLoadingSVG className="w-14 h-14 m-auto mt-16 text-inhaSkyBlue" />
+  ) : (
     <div>
-      <ul className="flex flex-col gap-y-2 p-2">
-        {threadList?.map((thread, idx) => (
-          <li key={thread.postId} className="flex flex-col gap-y-4 mb-2">
-            {idx > 0 && <div className="border w-full h-0"></div>}
-            <ThreadItem
-              thread={{
-                postId: thread.postId,
-                title: thread.title,
-                nickname: thread.users.nickname,
-                profileUrl: thread.users.profileUrl,
-                contents: thread.content,
-                date: new Date(thread.createdAt),
-                isLike: thread.checkLike,
-                isSave: thread.checkScrap,
-                likesCount: thread.likeNum,
-                commentsCount: thread.commentNum,
-                postImages: thread.postImages,
-              }}
-            />
-          </li>
-        ))}
-      </ul>
+      {threadList && threadList.length > 0 ? (
+        <ul className="flex flex-col gap-y-2 p-2">
+          {threadList?.map((thread, idx) => (
+            <li key={thread.postId} className="flex flex-col gap-y-4 mb-2">
+              {idx > 0 && <div className="border w-full h-0"></div>}
+              <ThreadItem
+                thread={{
+                  postId: thread.postId,
+                  title: thread.title,
+                  nickname: thread.users.nickname,
+                  profileUrl: thread.users.profileUrl,
+                  contents: thread.content,
+                  date: new Date(thread.createdAt),
+                  isLike: thread.checkLike,
+                  isSave: thread.checkScrap,
+                  likesCount: thread.likeNum,
+                  commentsCount: thread.commentNum,
+                  postImages: thread.postImages,
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="flex flex-col items-center gap-y-8 mt-10">
+          <img src={emptyBox} className="w-2/5 h-2/5 " />
+          <p className="font-bold text-2xl">There is nothing yet.</p>
+        </div>
+      )}
       <section ref={lastItemRef}>
         {hasNextPage ? (
           <BubbleLoadingSVG className="w-14 h-14 ml-auto mr-auto text-inhaSkyBlue" />
