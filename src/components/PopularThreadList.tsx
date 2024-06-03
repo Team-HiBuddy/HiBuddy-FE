187c0e9fff181f15c8ea6dd14b2e173b2fc2d29b
guridaek <xylitol79@gmail.com>
2024-06-03 13:29:40 +0900

    fix: popular thread가 없을 경우 렌더링 X

--- src/components/PopularThreadList.tsx
+++ src/components/PopularThreadList.tsx
@@ -5,25 +5,27 @@ function PopularThreadList() {
   const { data: threads } = usePopularThreads();
 
   return (
-    <div>
-      <h2 className="text-xl font-bold px-1 mb-2">Popular</h2>
+    threads && (
       <div>
-        <ul className="flex flex-col gap-2 border rounded border-inhaSkyBlue p-3">
-          {threads?.map(({ postId, title, likeNum, commentNum, checkLike }) => (
-            <PopularThreadItem
-              key={postId}
-              thread={{
-                postId,
-                title,
-                commentsCount: commentNum,
-                likesCount: likeNum,
-                isLike: checkLike,
-              }}
-            />
-          ))}
-        </ul>
+        <h2 className="text-xl font-bold px-1 mb-2">Popular</h2>
+        <div>
+          <ul className="flex flex-col gap-2 border rounded border-inhaSkyBlue p-3">
+            {threads.map(({ postId, title, likeNum, commentNum, checkLike }) => (
+              <PopularThreadItem
+                key={postId}
+                thread={{
+                  postId,
+                  title,
+                  commentsCount: commentNum,
+                  likesCount: likeNum,
+                  isLike: checkLike,
+                }}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    )
   );
 }
 
