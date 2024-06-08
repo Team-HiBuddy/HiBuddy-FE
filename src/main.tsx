6610f5dd89b67e19a92ab1bcbabab287b9564700
guridaek <xylitol79@gmail.com>
2024-06-08 18:10:42 +0900

    feat: msw development 환경일때만 활성화

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import { RouterProvider } from "react-router-dom";
 import { Router } from "./Router.tsx";
 
 export async function enableMocking() {
-  // if (process.env.NODE_ENV !== "development") {
-  //   return;
-  // }
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
 
   const { worker } = await import("./mocks/browser.ts");
 
