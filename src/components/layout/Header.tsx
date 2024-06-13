f9def192ef1fcf317914dc129644ab049d994d88
guridaek <xylitol79@gmail.com>
2024-06-13 17:03:11 +0900

    feat: Header 배경색, search 버튼 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,22 @@
 import ProfileSVG from "@assets/profile.svg?react";
+import SearchSVG from "@assets/search.svg?react";
 import { IconButton } from "@mui/material";
 import { ROUTER_PATH } from "../../routerConfig";
 import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="flex justify-between w-full h-16 sticky top-0 border-b-2 border-inhaSkyBlue p-4 bg-white z-10">
-      <Link className="text-xl font-bold text-inhaDeepBlue" to={ROUTER_PATH.MAIN}>
+    <header className="flex justify-between w-full h-16 sticky top-0 p-4 bg-inhaDeepBlue z-10 text-white">
+      <Link className="text-xl font-semibold" to={ROUTER_PATH.MAIN}>
         HiBuddy
       </Link>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-x-1">
+        <IconButton aria-label="search">
+          <SearchSVG className="text-white" />
+        </IconButton>
         <IconButton aria-label="profile">
           <Link to={ROUTER_PATH.MY_PAGE}>
-            <ProfileSVG />
+            <ProfileSVG className="text-white" />
           </Link>
         </IconButton>
       </div>
