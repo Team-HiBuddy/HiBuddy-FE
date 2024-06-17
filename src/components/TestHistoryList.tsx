d1907fde206f8e5f053fec71fb90d6631f465066
guridaek <xylitol79@gmail.com>
2024-06-17 13:28:23 +0900

    feat: 테스트 히스토리

--- src/components/TestHistoryList.tsx
+++ src/components/TestHistoryList.tsx
@@ -1,18 +1,24 @@
+import useTestHistory from "@hooks/query/koreanTest/useTestHistory";
 import TestHistoryItem from "./TestHistoryItem";
 
-const mocks = [
-  { id: "1", name: "Script 1", date: new Date("2024-05-17T12:34:56") },
-  { id: "2", name: "Script 3", date: new Date("2024-05-14T12:34:56") },
-  { id: "3", name: "Script 1", date: new Date("2024-05-12T12:34:56") },
-];
-
 function TestHistoryList() {
+  const { data } = useTestHistory();
+
+  if (data.length < 1) return null;
+
   return (
     <div className="flex flex-col gap-y-8">
       <h2 className="text-xl font-semibold">Test History</h2>
       <ul className="flex flex-col gap-y-5 w-full">
-        {mocks.map((history) => (
-          <TestHistoryItem key={history.id} history={history} />
+        {data.map((history) => (
+          <TestHistoryItem
+            key={history.testId}
+            history={{
+              testId: history.testId,
+              scriptName: history.scriptName,
+              date: new Date(history.testDate),
+            }}
+          />
         ))}
       </ul>
     </div>
